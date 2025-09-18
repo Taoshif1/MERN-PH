9f6ccf496a6676f32d88c810accd8cddc24d6826
Taoshif1 <taoshif2@gmail.com>
2025-09-18 14:50:22 +0600

    readme added

---
+++ docs/script.js
@@ -0,0 +1,21 @@
+
+// login btn functionality
+const loginBtn = document.getElementById("loginBtn").addEventListener("click", ()=>{
+    // fixed num & pin
+    const mobileNum = 12345
+    const pin = 1234
+
+    const formMobileNumber = document.getElementById("mobileNum").value
+    const convertedNum = parseInt(formMobileNumber)
+
+    const formPin = document.getElementById("pin").value
+    const convertedPin = parseInt(formPin)
+    // console.log(`Value: ${formMobileNumber.value}, PIN: ${formPin.value}`)
+
+    if(convertedNum === mobileNum && convertedPin === pin){
+        window.location.href="./home.html"
+    }else{
+        alert("Wrong Credentials")
+    }
+})
+
