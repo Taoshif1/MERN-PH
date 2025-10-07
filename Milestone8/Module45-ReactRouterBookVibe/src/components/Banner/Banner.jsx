0a61f89e024ea182c4af67132d6948494e4102c5
Taoshif1 <taoshif2@gmail.com>
2025-10-07 19:52:28 +0600

    Books.jsx added

--- Milestone8/Module45-ReactRouterBookVibe/src/components/Banner/Banner.jsx
+++ Milestone8/Module45-ReactRouterBookVibe/src/components/Banner/Banner.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import bookimage from '../../assets/books.jpg'
+
 const Banner = () => {
     return (
-        <div>
+        <div className='flex justify-around items-center p-10 w-full'>
             <div>
                 <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptatibus perferendis autem consectetur cupiditate labore commodi atque porro quibusdam nihil.</h1>
                 <button className="btn btn-primary">Test test</button>
             </div>
             <div>
-                <img src="" alt="" />
+                <img className='w-4/12' src={bookimage} alt="" />
             </div>
         </div>
     );
