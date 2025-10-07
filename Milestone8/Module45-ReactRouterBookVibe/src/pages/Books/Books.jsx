dc3e0ad54ba2362295fbe70c1b46941fa4a985b6
Taoshif1 <taoshif2@gmail.com>
2025-10-07 19:59:39 +0600

    bookData.json fetched

--- Milestone8/Module45-ReactRouterBookVibe/src/pages/Books/Books.jsx
+++ Milestone8/Module45-ReactRouterBookVibe/src/pages/Books/Books.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Books = () => {
+    const [allBooks, setAllBooks] = useState([]);
+
+    useEffect(()=>{
+        fetch("booksData.json")
+        .then(res => res.json())
+        .then(data =>
+            console.log(data)
+        )
+    },[])
     return (
         <div>
             <h1>Hello ami books</h1>
