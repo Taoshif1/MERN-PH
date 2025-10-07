255ff886cc2916a5eb73731ca753328f9d8200da
Taoshif1 <taoshif2@gmail.com>
2025-10-07 19:27:53 +0600

    banner component & home page added

---
+++ Milestone8/Module45-ReactRouterBookVibe/src/components/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div>
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptatibus perferendis autem consectetur cupiditate labore commodi atque porro quibusdam nihil.</h1>
+                <button className="btn btn-primary">Test test</button>
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- Milestone8/Module45-ReactRouterBookVibe/src/pages/Root/Root.jsx
+++ Milestone8/Module45-ReactRouterBookVibe/src/pages/Root/Root.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Navbar from '../../components/Header/Navbar';
 import Footer from '../../components/Footer/Footer';
 
 const Root = () => {
     return (
-        <div>
+        <div className='max-w-7xl mx-auto'>
             <Navbar></Navbar>
+            <Outlet></Outlet>
             <Footer></Footer>
         </div>
     );
