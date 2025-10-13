5f754623d622f9705d94bbff67872cb6bebba137
Taoshif1 <taoshif2@gmail.com>
2025-10-13 18:38:28 +0600

    Header Home Root Router created

---
+++ Milestone9/Module47SimpleReactFirebaseAuth/src/layout/Root.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Root = () => {
+  return <div>hi i am root</div>;
+};
+
+export default Root;
--- Milestone9/Module47SimpleReactFirebaseAuth/src/main.jsx
+++ Milestone9/Module47SimpleReactFirebaseAuth/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from "./layout/Root.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      
+    ]
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
