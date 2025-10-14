eb5d3b149ddda332645c857fd36be9899fe24849
Taoshif1 <taoshif2@gmail.com>
2025-10-14 14:22:45 +0600

    __info__.js added

---
+++ Milestone9/Module48EmailPassLoginAuth/src/components/__info__.js
@@ -0,0 +1,7 @@
+/**
+ * 1. First time visit? No account before -> Sign up == create acc == register
+ * 
+ * 2. Already have an account? Second time visit
+ * -> Sign in == log in == authenticate
+ * 
+ */
