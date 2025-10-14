5373d2d9e4694b6f523ec771d5393364209707f5
Taoshif1 <taoshif2@gmail.com>
2025-10-14 19:21:18 +0600

    login, register completed

--- Milestone9/Module48EmailPassLoginAuth/src/components/Login/Login.jsx
+++ Milestone9/Module48EmailPassLoginAuth/src/components/Login/Login.jsx
@@ -1,29 +1,65 @@
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import React from 'react';
+import { Link } from 'react-router';
+import { auth } from '../../firebase/firebase.init';
+import { useState } from 'react';
 
 const Login = () => {
+
+    const [error, setError] = useState("");
+
+    const handleLogin = e => {
+        e.preventDefault();
+        
+        const email = e.target.email.value; //get value by name from FORM
+        const password = e.target.password.value; //get value by name from FORM
+
+        // console.log("mail & pass- ", email, password);
+        setError("");
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            // Signed in
+            const user = result.user;
+            console.log('Logged in user: ', user);
+        })
+        .catch(error => {
+            console.error(error.message);
+            setError(error.message);
+        });
+    }
+
+
+
     return (
-        <div class="hero bg-base-200 min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <div class="text-center lg:text-left">
-                    <h1 class="text-8xl font-bold">Login Now!</h1>
-                    
-                </div>
-                <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div class="card bg-base-100 w-full m-auto max-w-sm shrink-0 shadow-2xl">
                     <div class="card-body">
-                        <fieldset class="fieldset">
-                            <label class="label">Email</label>
-                            <input type="email" class="input" placeholder="Email" />
-                            <label class="label">Password</label>
-                            <input type="password" class="input" placeholder="Password" />
-                                <div>
-                                    <a class="link link-hover">Forgot password?</a>
-                                </div>
-                            <button class="btn btn-neutral mt-4">Login</button>
-                        </fieldset>
+                    <h1 class="text-6xl font-bold mb-6">Login Now!</h1>
+                        <form onSubmit={handleLogin}>
+                            <fieldset class="fieldset">
+                                <label class="label">Email</label>
+                                <input type="email" class="input" name="email" placeholder="Email" />
+                            
+                                <label class="label">Password</label>
+                                <input type="password" class="input" name="password" placeholder="Password" />
+                                    <div>
+                                        <a class="link link-hover">Forgot password?</a>
+                                    </div>
+                                <button class="btn btn-neutral mt-4">Login</button>
+                            </fieldset>
+                        </form>
+                        {
+                            error && <p className='text-red-600'>{error}</p>
+                        }
+                        <p class="text-lg">
+                            New to Our Website? Please
+                            <Link to="/register" class="link link-hover text-blue-600 font-bold ml-1 text-xl">
+                                Register
+                            </Link>
+                        </p>
                     </div>
                 </div>
-            </div>
-        </div>
+
     );
 };
 
--- Milestone9/Module48EmailPassLoginAuth/src/components/Register/Register.jsx
+++ Milestone9/Module48EmailPassLoginAuth/src/components/Register/Register.jsx
@@ -1,43 +1,86 @@
 import React, { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
+import { Link } from 'react-router';
+
+
+import { FaEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
 
 const Register = () => {
 
-    const [user, setUser] = useState(false);
-    const [error, setError] = useState('');
+    const [user, setUser] = useState(false);  //for success message when user is created
+    const [error, setError] = useState('');  //for error message
+    const [showPassword, setShowPassword] = useState(false); //for show/hide password
 
     const handleRegister = (e) =>{
+        e.preventDefault(); // Prevents default form submission/stops reloading
+      // register logic here
+        const email = e.target.email.value; //get value by name from FORM
+        const password = e.target.password.value; //get value by name from FORM
+        const terms = e.target.terms.checked; //get checkbox value by name from FORM
+        console.log("Register button clicked, ", email, password, terms);
 
-        e.preventDefault();  // Prevents default form submission/stops reloading
-        // register logic here
-        const email = e.target.email.value;    //get value by name from FORM
-        const password = e.target.password.value;  //get value by name from FORM
-        console.log("Register button clicked, ", email, password);
-        
-        // reset error message
-        setError('');  //by default empty
+      // password validation using REGULAR EXPRESSION (regex)
+        const passwordLengthPattern = /^.{6,}$/; // validation 1 - minimum 6 characters
+        const casePattern = /^(?=.*[a-z])(?=.*[A-Z]).+$/; // validation 2 - at least 1 upper & 1 lower
+        const specialCharPattern = /^(?=.*[!@#$%^&*(),.?":{}|<>]).+$/; // validation 3 - at least 1 special character
+        // const allPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{6,}$/; // all in one
 
-        //setUser(false); //by default false
-        setUser(false);
 
-        // validation
-        createUserWithEmailAndPassword(auth, email, password)
-            .then(result => {
-                console.log("after creation - ", result.user)
-                setUser(true);
-                e.target.reset(); // reset form after submission
-            })
-            .catch(error => {
-                console.log('Error during registration:', error);
-                setError(error.message);
-            })
+      //validation 1 - length
+        if (!passwordLengthPattern.test(password)) {
+            setError("Error: Password must be at least 6 characters long");
+            console.log("pass didnt matched");
+        return;
+    }
+        //validation 2 - case
+        else if (!casePattern.test(password)) {
+            setError(
+            "Error: Password must contain at least one uppercase and one lowercase letter"
+            );
+        return;
+    }
+
+    else if (!specialCharPattern.test(password)) {
+        setError("Error: Password must contain at least one special character (e.g. ! @ # $ % ^ & *)");
+        return;
+    }
+
+      // reset error message
+      setError(""); //by default empty
+
+      //setUser(false); //by default false
+    setUser(false);
+
+    // terms and conditions validation
+    if(!terms){
+        setError("Please accept our terms and conditions");
+        return;
+    }
+
+      // validation
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+        console.log("after creation - ", result.user);
+        setUser(true);
+          e.target.reset(); // reset form after submission
+        })
+        .catch((error) => {
+        console.log("Error during registration:", error);
+        setError(error.message);
+        });
+    }
+
+    const handleTogglePassShow = (e) =>{
+        e.preventDefault();
+        setShowPassword(!showPassword);
     }
 
 
     return (
         <div class="hero bg-base-200 min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
+            <div class="hero-content flex-col lg:flex-row-reverse ">
                 <div class="text-center lg:text-left">
                     <h1 class="text-8xl font-bold">Register Now!</h1>
                 </div>
@@ -49,14 +92,37 @@ const Register = () => {
                                 <input type="email" class="input" placeholder="Email" name="email"/>
 
                                 <label class="label">Password</label>
-                                <input type="password" class="input" placeholder="Password" name="password"/>
+                                <div className='relative'>
+                                    <input type={showPassword ? "text" : "password"} class="input" placeholder="Password" name="password"/>
+                                    <button 
+                                    onClick={handleTogglePassShow}
+                                    class="btn btn-xs absolute -ml-11 mt-1.5 ">
+                                        {showPassword ? <FaRegEyeSlash /> : <FaEye />}
+                                    </button>
+                                </div>
+
+                                <div>
+                                    <label class="label">
+                                    <input type="checkbox" name='terms'
+                                    class="checkbox" />
+                                    Accept Our Terms & Conditions
+                                    </label>
+                                </div>
+                                <p>Forgot Password?
+                                    <a href="/forgot-password" class="link link-hover text-blue-600 font-bold ml-1">Reset here</a>
+                                </p>
+
                                 <div>
                                     <p class="text-lg">
-                                    Already Have an Account?
-                                        <a class="link link-hover text-blue-600 font-bold ml-1 text-xl">
+                                    Already Have an Account? Please
+                                        {/* <a  class="link link-hover text-blue-600 font-bold ml-1 text-xl">
                                             Login
-                                        </a>
+                                        </a> */}
+                                        <Link to="/login" class="link link-hover text-blue-600 font-bold ml-1 text-xl">
+                                            Login
+                                        </Link>
                                     </p>
+                                    
                                 </div>
                                 <button class="btn btn-neutral mt-4">Register</button>
                             </fieldset>
