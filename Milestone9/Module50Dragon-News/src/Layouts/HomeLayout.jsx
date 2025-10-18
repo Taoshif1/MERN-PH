46312bcab92d8e8ce39fe3d291fe526443d52c8e
Taoshif1 <taoshif2@gmail.com>
2025-10-18 18:45:21 +0600

    header completed with date & Marquee

--- Milestone9/Module50Dragon-News/src/Layouts/HomeLayout.jsx
+++ Milestone9/Module50Dragon-News/src/Layouts/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../components/Header';
+import LatestNews from '../components/LatestNews';
 
 const HomeLayout = () => {
     return (
         <div>
             <header>
                 <Header></Header>
+                <section className='w-11/12 mx-auto my-4'>
+                    <LatestNews></LatestNews>
+                </section>
             </header>
 
             <main>
@@ -16,7 +20,7 @@ const HomeLayout = () => {
                     </section>
                     <section className="right_nav"></section>
             </main>
-            
+
         </div>
     );
 };
--- Milestone9/Module50Dragon-News/src/components/Header.jsx
+++ Milestone9/Module50Dragon-News/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from "../assets/logo.png"
+import { format} from "date-fns";
 
 const Header = () => {
     return (
         <div className='flex flex-col justify-center items-center gap-3'>
-            <img className='w-[350px]' src={logo} alt="" />
+            <img className='w-[450px]' src={logo} alt="" />
             {/* <h1>This is header</h1> */}
-            <p>Journalism without Fear or Favour</p>
+            <p className='text-accent'>Journalism without Fear or Favour</p>
+            
+            {/* Date added dynamically using date-fns */}
+            <p className='text-semibold text-accent'>{ 
+                format(new Date(), "EEEE, MMMM MM, yyyy")
+                }</p>
         </div>
     );
 };
