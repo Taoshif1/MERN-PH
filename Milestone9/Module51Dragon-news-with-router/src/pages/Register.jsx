9826e106165f92b969bea72ea4bf4604cd0eaffa
Taoshif1 <taoshif2@gmail.com>
2025-10-21 14:41:15 +0600

    Auth imported & user register done

--- Milestone9/Module51Dragon-news-with-router/src/pages/Register.jsx
+++ Milestone9/Module51Dragon-news-with-router/src/pages/Register.jsx
@@ -1,33 +1,60 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../provider/AuthProvider';
 
 const Register = () => {
+    const {createUser, setUser} = use(AuthContext);
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const photourl = form.photourl.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log({name, photourl, email, password});
+        createUser(email, password)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+            setUser(user);
+        })
+        .catch((error)=>{
+            const errorCode = error.code;  
+            const errorMessage = error.message;  
+            alert(errorMessage);
+        })
+    }
+
     return (
         <div className='flex justify-center min-h-screen items-center'>
             <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl ">
                 <h2 className='font-semibold text-2xl text-center pt-8 '>Register your account</h2>
-                <div class="card-body">
+                
+                <form onSubmit={handleRegister} class="card-body">
                     <fieldset class="fieldset">
 
                         <label class="label">Name</label>
-                        <input type="text" class="input" placeholder="Name" />
+                        <input name='name' type="text" class="input" placeholder="Name" required />
                         
                         <label class="label">Photo URL</label>
-                        <input type="email" class="input" placeholder="Photo URL" />
+                        <input name='photourl' type="email" class="input" placeholder="Photo URL required" />
                         
                         <label class="label">Email</label>
-                        <input type="email" class="input" placeholder="Email" />
+                        <input name='email' type="email" class="input" placeholder="Email" required />
 
                         <label class="label">Password</label>
-                        <input type="password" class="input" placeholder="Password" />
+                        <input name='password' type="password" class="input" placeholder="Password" required />
                             
-                        <button class="btn btn-neutral mt-4">Register</button>
+                        <button type='submit' class="btn btn-neutral mt-4">Register</button>
+                        
                         <p className='font-semibold text-center pt-5'>
                             Already Have An Account ?
                             <Link to="/auth/Login" className='text-secondary'> Login</Link>
                         </p>
                     </fieldset>
-                </div>
+                </form>
+
             </div>
         </div>
     );
--- Milestone9/Module51Dragon-news-with-router/src/provider/AuthProvider.jsx
+++ Milestone9/Module51Dragon-news-with-router/src/provider/AuthProvider.jsx
@@ -1,22 +1,43 @@
 import React, { createContext, useState } from 'react';
+import app from '../firebase/firebase.config';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from 'react';
 
 // Step 1 - Create a context
 export const AuthContext = createContext();
 
+// Auth from firebase to handle login logout
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 // AuthProvider er peter vitore RouterProvider in main.jsx tai ekhane {children} diye destructure kore nisi
 const AuthProvider = ({children}) => {
 
     // State for our user
-    const [user, setUser] = useState({
-        name: "Taoshif Gazi",
-        email: "taoshif2@gmail.com",
-    })
+    const [user, setUser] = useState(null);
+
+    // func to signup user
+    const createUser = (email, password) => {
+
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    // observer
+    useEffect(()=>{
+        //sets users data
+        const unsubscribe = onAuthStateChanged(auth,(currentUser) => {
+            setUser(currentUser);
+        });
+        return ()=>{
+            unsubscribe();
+        }
+    },[])
 
     // Displayed via <AuthContext /> prop (value={authData})
     const authData = {
         user:user,  // same as writing "user" only like the below one
         setUser,  // setUser:setUser (same)
+        createUser,
     }
 
     return (
