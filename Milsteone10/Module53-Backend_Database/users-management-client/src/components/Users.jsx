3971263d75e240937d3a0fe06dad386724e81671
Taoshif1 <taoshif2@gmail.com>
2025-10-28 14:41:29 +0600

    users data loaded from server to client

--- Milsteone10/Module53-Backend_Database/users-management-client/src/components/Users.jsx
+++ Milsteone10/Module53-Backend_Database/users-management-client/src/components/Users.jsx
@@ -7,7 +7,26 @@ const Users = ({usersPromise}) => {
 
 
   return (
-    <div>  </div>
+    <div>
+        <div>
+            <h3>Add a user</h3>
+            <form>
+                <input type="text" placeholder='name'/>
+                <br />
+                <input type="email" placeholder='email'/>
+                <br />
+                <button>Add User</button>
+            </form>
+        </div>
+        <div>
+            {
+            users.map(user => 
+            <p key={user.id}>
+                {user.name} - {user.email}
+            </p>)
+        }    
+        </div>
+    </div>
   )
 }
 
