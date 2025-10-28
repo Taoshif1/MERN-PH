b4e252762b003f6220099153690ba385f4c999d3
Taoshif1 <taoshif2@gmail.com>
2025-10-28 14:17:40 +0600

    clinet added

--- Milsteone10/Module53-Backend_Database/user-management-server/index.js
+++ Milsteone10/Module53-Backend_Database/user-management-server/index.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) => {
     res.send('user server is available');
 })
 
+const users = [
+    {id:1, name: "Mahafuza", email: "mahafuza@gmail.com"},
+    {id:2, name: "Taoshif", email: "taoshif@gmail.com"},
+    {id:3, name: "noone", email: "noone@gmail.com"},
+]
+
+app.get('/users', (req, res)=>{
+    res.send(users);
+})
+
 app.listen(port, ()=>{
     console.log(`user server started on PORT ${port}`)
 })
