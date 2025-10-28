85da99ba552aa580b0e4d9cd6951d57b007c1efe
Taoshif1 <taoshif2@gmail.com>
2025-10-28 16:19:10 +0600

    completed

--- Milsteone10/Module53-Backend_Database/user-management-server/index.js
+++ Milsteone10/Module53-Backend_Database/user-management-server/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000 ;
 
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('user server is available');
@@ -18,6 +19,14 @@ const users = [
 
 app.get('/users', (req, res)=>{
     res.send(users);
+})
+
+app.post('/users', (req, res) =>{
+    console.log('post method called', req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
 })
 
 app.listen(port, ()=>{
--- Milsteone10/Module53-Backend_Database/users-management-client/src/components/Users.jsx
+++ Milsteone10/Module53-Backend_Database/users-management-client/src/components/Users.jsx
@@ -1,19 +1,45 @@
 import React from 'react'
+import { useState } from 'react';
 import { use } from 'react'
 
 const Users = ({usersPromise}) => {
-    const users = use(usersPromise);
-    console.log(users);
+    const initialUsers = use(usersPromise);
+    const [users, setUsers] = useState(initialUsers);
 
+    const handleAddUser = (e) =>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+
+        console.log(name, email);
+
+        const newUser  = { name, email};
+
+        // Send data to server
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+            .then(res => res.json())
+            .then(data =>{
+                console.log("after post - ", data);
+                const newUsers = [...users, data];
+                setUsers(newUsers);
+                e.target.reset();
+            })
+    }
 
   return (
     <div>
         <div>
             <h3>Add a user</h3>
-            <form>
-                <input type="text" placeholder='name'/>
+            <form onSubmit={handleAddUser}>
+                <input name="name" id=" " type="text" placeholder='name'/>
                 <br />
-                <input type="email" placeholder='email'/>
+                <input name="email" id=" " type="email" placeholder='email'/>
                 <br />
                 <button>Add User</button>
             </form>
@@ -30,4 +56,15 @@ const Users = ({usersPromise}) => {
   )
 }
 
-export default Users
+export default Users;
+
+/**
+ * 0. have to send request object to the server
+ * 1. mention method: post
+ * 2. mention header: about JSON data in the property of content-type:application/json
+ * 3. body: JSON.stringify(newUser)
+ * 
+ * -------------------------------------
+ * on the server side use json as middleware
+ * app.use(express.json())
+ */
