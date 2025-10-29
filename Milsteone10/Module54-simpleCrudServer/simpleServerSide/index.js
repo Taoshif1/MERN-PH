4105bcd4887009ad07abe3650a75f46752354bdb
Taoshif1 <taoshif2@gmail.com>
2025-10-29 15:20:22 +0600

    mongoDB atlas connected & server done

---
+++ Milsteone10/Module54-simpleCrudServer/simpleServerSide/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongoDB: simpleDBUser - d1F69hx7hPo2bzUA
+const uri = "mongodb+srv://simpleDBUser:d1F69hx7hPo2bzUA@cluster0.kpmcxd4.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get('/', (req, res)=>{
+    res.send(`Simple CRUD server is running on WEB PORT: ${port}`);
+})
+
+
+// async function run() {
+//     await
+// }
+// run().catch(console.dir)
+
+async function run() {
+    try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+    }
+}
+run().catch(console.dir)
+
+
+
+app.listen(port, ()=>{
+    console.log(`Simple CRUD Server Running on Terminal PORT: ${port}`);
+})
+
+
+
+/**
+ * 1. at least one user
+ * 2. set uri with userId & password
+ * 3. create a mongodb client
+ * 4. add a run function to connect to the database
+ * 5. use try finally inside it to connect the client
+ * 6. ping the database to see if server is alive or not
+ * 
+ */
+
+
+
+
+
+
+
