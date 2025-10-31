beb25780e4c4ac9a7d08784fc1dd2691c66ed912
Taoshif1 <taoshif2@gmail.com>
2025-10-31 21:25:15 +0600

    smart deals server project created

---
+++ Milsteone10/Module55-SmartDealsMongoDB/smart-deals-server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Smart ZDeals Server ")
+})
+
+app.listen(port, ()=>{
+    console.log(`Smart Server Running on Port: ${port}`);
+})
+
+
+
+
