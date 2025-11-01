759ce7983c326af7263371fc92bdfabbee4efc2d
Taoshif1 <taoshif2@gmail.com>
2025-11-01 21:03:37 +0600

    Register & Social Login Feature Implemented

--- Milsteone10/Module55-SmartDealsMongoDB/smart-deals-client/src/main.jsx
+++ Milsteone10/Module55-SmartDealsMongoDB/smart-deals-client/src/main.jsx
@@ -8,6 +8,9 @@ import RootLayout from './layouts/RootLayout.jsx'
 import Home from './components/Home/Home.jsx';
 import AllProducts from './components/AllProducts/AllProducts.jsx';
 import AuthProvider from './context/AuthProvider.jsx';
+import Register from './components/Register/Register.jsx';
+import MyProducts from './components/MyProducts/MyProducts.jsx';
+import MyBids from './components/MyBids/MyBids.jsx';
 
 const router = createBrowserRouter([
   {
@@ -21,6 +24,18 @@ const router = createBrowserRouter([
       {
         path: 'allProducts',
         Component: AllProducts
+      },
+      {
+        path: 'register',
+        Component: Register
+      },
+      {
+        path: 'myProducts',
+        element: <MyProducts></MyProducts>
+      },
+      {
+        path: 'myBids',
+        element: <MyBids></MyBids>
       },
     ]
 
