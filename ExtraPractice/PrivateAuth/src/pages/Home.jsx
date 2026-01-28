99d721cc8d8f0e369ac26a96732b76e2e17cec45
Taoshif1 <taoshif2@gmail.com>
2026-01-28 21:20:11 +0600

    router added, navbar done & login, register created

---
+++ ExtraPractice/PrivateAuth/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar';
+import React from 'react';
+import App from '../App';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <App></App>
+        </div>
+
+    );
+};
+
+export default Home;
