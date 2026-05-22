c0e13023069a08c2120145b1b1f114e1b573fed5
Taoshif1 <taoshif2@gmail.com>
2026-05-22 14:21:29 +0600

    added first typescript code

---
+++ TypeScript/basics/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName = "Taoshif";
+let age = 21;
+let isStudent = true;
+console.log(`My Name is ${userName}, i'm ${age} years old & always speak ${isStudent}`);
+//# sourceMappingURL=index.js.map
---
+++ TypeScript/basics/index.ts
@@ -0,0 +1,5 @@
+let userName: string = "Taoshif";
+let age: number = 21;
+let isStudent: boolean = true;
+
+console.log(`My Name is ${userName}, i'm ${age} years old & always speak ${isStudent}`)
