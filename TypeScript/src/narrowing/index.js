c7fb1c2c15f98e202229cdbbacaa8bf37d8d2489
Taoshif1 <taoshif2@gmail.com>
2026-05-23 21:22:37 +0600

    narrowing concept learnt & hands-on coded

--- TypeScript/src/narrowing/index.js
+++ TypeScript/src/narrowing/index.js
@@ -1,3 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello, World!");
+// 1️⃣ Type Guards & Type Narrowing
+function formatValue(value) {
+    if (typeof value === "string") {
+        return value.toUpperCase();
+    }
+    else {
+        return value.toFixed(2);
+    }
+}
+// console.log(formatValue("hello"));
+// console.log(formatValue(123.456));
+// 2️⃣ Truthiness Narrowing
+function printMessage(message) {
+    if (message) {
+        console.log(message.toUpperCase());
+    }
+    else {
+        console.log("No message provided.");
+    }
+}
+// printMessage("Hello, TypeScript!");
+// printMessage(null);
+// 3️⃣ instanceof Narrowing
+class Dog {
+    bark() {
+        console.log("Bark bark!");
+    }
+}
+class Cat {
+    meow() {
+        console.log("Meow meow!");
+    }
+}
+function makeSound(animal) {
+    if (animal instanceof Dog) {
+        animal.bark();
+    }
+    else {
+        animal.meow();
+    }
+}
+const dog = new Dog();
+const cat = new Cat();
+makeSound(dog);
+makeSound(cat);
--- TypeScript/src/narrowing/index.ts
+++ TypeScript/src/narrowing/index.ts
@@ -1,1 +1,53 @@
-console.log("Hello, World!");
+// 1️⃣ Type Guards & Type Narrowing
+function formatValue(value: string | number) {
+    if (typeof value === "string") {
+        return value.toUpperCase();
+    } else {
+        return value.toFixed(2);
+    }
+}
+
+// console.log(formatValue("hello"));
+// console.log(formatValue(123.456));
+
+
+// 2️⃣ Truthiness Narrowing
+function printMessage(message: string | null){
+    if (message) {
+        console.log(message.toUpperCase());
+    }
+    else {
+        console.log("No message provided.");
+    }
+}
+
+// printMessage("Hello, TypeScript!");
+// printMessage(null);
+
+
+// 3️⃣ instanceof Narrowing
+class Dog {
+    bark() {
+        console.log("Bark bark!");
+    }
+}
+
+class Cat {
+    meow() {
+        console.log("Meow meow!");
+    }
+}
+
+function makeSound(animal: Dog | Cat) {
+    if (animal instanceof Dog) {
+        animal.bark();
+    } else {
+        animal.meow();
+    }
+}
+
+const dog = new Dog();
+const cat = new Cat();
+
+makeSound(dog);
+makeSound(cat);
